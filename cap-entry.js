4f77c7d4104060a411ea7bf00084db53f47efbc0
sengakoji <100504585+sengakoji@users.noreply.github.com>
2026-03-04 11:47:25 +0900

    fix(native): resolve overlapping status bar and apply dynamic theme color

--- cap-entry.js
+++ cap-entry.js
@@ -1,6 +1,8 @@
 import { Capacitor } from '@capacitor/core';
 import { LocalNotifications } from '@capacitor/local-notifications';
+import { StatusBar } from '@capacitor/status-bar';
 
 window.Capacitor = Capacitor;
 window.Capacitor.Plugins = window.Capacitor.Plugins || {};
 window.Capacitor.Plugins.LocalNotifications = LocalNotifications;
+window.Capacitor.Plugins.StatusBar = StatusBar;
